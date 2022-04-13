7e8f388a43df38efe0525b98d213994a6ad9b457
Jesús Monge <jesus_jaml@outlook.com>
2022-04-12 21:41:37 -0500

    Developed responsive menu mobile

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 // Query selectors Variables
 const $menuBtn = document.querySelector('.menu-btn')
+const $navigator = document.getElementById('nav-menu')
+const $navLink = document.querySelectorAll('.nav__link')
 
-// toggle open class when clicked $menuBtn button
+// toggle open class and show menu when clicked $menuBtn button
 const toggleMenuBtn = () => {
   $menuBtn.classList.toggle('open')
+  $menuBtn.classList.contains('open')
+    ? $navigator.classList.add('show')
+    : $navigator.classList.remove('show')
 }
 
 // addEventListener to listen $menuBtn click and run the function toggleMenuBtn
 $menuBtn.addEventListener('click', toggleMenuBtn)
+
+// Active and remove menu
+function linkAction() {
+  $navLink.forEach((n) => n.classList.remove('active'))
+  this.classList.add('active')
+
+  // close menu mobile when clicked item
+  $navigator.classList.remove('show')
+  $menuBtn.classList.toggle('open')
+}
+
+$navLink.forEach((n) => n.addEventListener('click', linkAction))
